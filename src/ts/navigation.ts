9056ae08cc56310e3f4961a26fa162d9a4b6c684
Louie Heaton <louie@hatcha.co.uk>
2023-05-28 09:46:50 +0100

    fix: pushstate

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -16,6 +16,7 @@ const wpApp = {
 
         if (this.pageCache[route]) {
             this.currentRoute = route;
+            this.updateURL(route);
         } else {
             fetchPage(route)
                 .then((response) => response.json())
@@ -23,16 +24,25 @@ const wpApp = {
                     console.log(data);
                     this.pageCache[route] = data[0];
                     this.currentRoute = route;
+                    this.updateURL(route);
                 });
         }
 
-        this.updateURL(route);
     },
 
     updateURL(route: string) {
+        //* Get current route
         const url = route[0] != '/' ? `/${route}` : route;
-        console.log('url', url);
-        window.history.replaceState(null, null, url);
+        const locPath = window.location.pathname;
+
+        //* Workout current location
+        let pathname = locPath.replace(/\/$/, '');
+        pathname = (pathname == '') ? '/' : '/' + pathname.split('/').pop();
+
+        //* If then Push
+        if (pathname != url) {
+            window.history.pushState(null, null, url);
+        }
     },
 };
 
